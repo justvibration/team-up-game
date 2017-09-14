e67543c681a207766eb95283c82f6213a338d044
Jim Engman <jim.engman@scania.com>
2017-09-14 16:10:27 +0200

    improvements

--- js/game.js
+++ js/game.js
@@ -10,13 +10,12 @@ var game = new Phaser.Game(
   }
 );
 
+// load assets
 function preload() {
-
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-
 }
 
 var player;
