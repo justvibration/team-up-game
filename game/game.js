7bb3efde372d8195a3a8955284ef82953c62b8b1
msufufjr <hogaborgare@gmail.com>
2017-09-14 15:46:19 +0200

    world stuff

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(
-  '800', // width
-  '600', // height
+  '1500', // width
+  '770', // height
   Phaser.AUTO, // rendering context (WebGL or Convas)
   'game', // DOM id
   {
@@ -12,7 +12,7 @@ var game = new Phaser.Game(
 
 function preload() {
 
-  game.load.image('sky', 'assets/sky.png');
+  game.load.image('bg', 'assets/bg.jpg');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
@@ -33,7 +33,7 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   //  A simple background for our game
-  game.add.sprite(0, 0, 'sky');
+  game.add.sprite(0, 0, 'bg');
 
   //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = game.add.group();
@@ -42,10 +42,10 @@ function create() {
   platforms.enableBody = true;
 
   // Here we create the ground.
-  var ground = platforms.create(0, game.world.height - 64, 'ground');
+  var ground = platforms.create(0, game.world.height-32, 'ground');
 
   //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  ground.scale.setTo(2, 2);
+  ground.scale.setTo(4, 1);
 
   //  This stops it from falling away when you jump on it
   ground.body.immovable = true;
@@ -65,7 +65,7 @@ function create() {
 
   //  Player physics properties. Give the little guy a slight bounce.
   player.body.bounce.y = 0.2;
-  player.body.gravity.y = 300;
+  player.body.gravity.y = 500;
   player.body.collideWorldBounds = true;
 
   //  Our two animations, walking left and right.
